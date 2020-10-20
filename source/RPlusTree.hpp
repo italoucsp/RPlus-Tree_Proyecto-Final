35591d936dd2bcb21a785d98f1cf0ff4a9c22a01
italoucsp <42581127+italoucsp@users.noreply.github.com>
2020-10-20 01:11:06 -0500

    Basic Structure
    Nothing functional yet.

--- source/RPlusTree.hpp
+++ source/RPlusTree.hpp
@@ -1,1 +1,69 @@
 #include <rplus_utils.hpp>
+
+//DIMENSIONS, MAX NUMBER OF ENTRIES PER NODE
+template<typename DATA_TYPE, int N_,int M_>
+class RPlus {
+  size_t N, M, m;
+
+  struct Node;
+
+  struct Entry {
+    HyperRectangle<N_> entry_rect;
+    DATA_TYPE data;
+    shared_ptr<Node> child;
+  };
+
+  struct Node {
+    HyperRectangle<N_> node_rect;
+    ENTRY_GROUP entries;
+
+    Node(size_t M);
+  };
+
+  shared_ptr<Node> root;
+
+  void hidden_search(shared_ptr<Node> &R, const HyperRectangle<N_> &W);
+  void split_node(shared_ptr<Node> &R);
+  pair<shared_ptr<Node>, RECT_GROUP> partition(RECT_GROUP &S, unsigned int ff);
+  pair<double,DATA_TYPE> sweep(size_t axis, DATA_TYPE Okd, unsigned int ff);
+  void pack(RECT_GROUP &S, unsigned int ff);
+public:
+  RPlus();
+  ~RPlus();
+  void search(const HyperRectangle<N_> &W);
+  void kNN_query(size_t k);
+  void insert(shared_ptr<Node> &R, const HyperRectangle<N_> &IR);
+  void remove(shared_ptr<Node> &R, const HyperRectangle<N_> &IR);
+};
+
+//===============================R-PLUS-TREE-IMPLEMENTATION============================================
+
+template<typename T, int N_, int M_>
+RPlus<T, N_, M_>::RPlus() {
+  try {
+    if (N_ < 2 || M_ < 2) {
+      throw runtime_error(ERROR_M_N_VALUES);
+    }
+    else {
+      M = size_t(M_);
+      N = size_t(N_);
+      m = M / 2;
+      root = make_shared<Node>(M);
+    }
+  }
+  catch(const exception &error){
+    ALERT(error.what());
+  }
+}
+
+template<typename T, int N_, int M_>
+RPlus<T, N_, M_>::~RPlus() {
+  root.reset();
+}
+
+//========================================NODE-IMPLEMENTATION==========================================
+
+template<typename T, int N_, int M_>
+RPlus<T, N_, M_>::Node::Node(size_t M) {
+  entries.resize(M);
+}
--- source/rplus_utils.hpp
+++ source/rplus_utils.hpp
@@ -2,5 +2,29 @@
 #include <memory>
 #include <algorithm>
 #include <vector>
+#include <stdexcept>
+#include <utility>
 
 using namespace std;
+
+#define ENTRY_GROUP vector<Entry>
+#define RECT_GROUP vector<HyperRectangle<N_>>
+#define ALERT(message) cout << message << endl;
+#define ERROR_M_N_VALUES "ERROR: El n\243mero de dimensiones y el n\243mero de espacios por nodo deben ser mayor a 1."
+
+
+template<int N>
+struct HyperRectangle {
+  HyperRectangle();
+  bool isOverlaping(const HyperRectangle &other);
+};
+
+template<int N>
+HyperRectangle<N>::HyperRectangle() {
+
+}
+
+template<int N>
+bool HyperRectangle<N>::isOverlaping(const HyperRectangle &other) {
+  return true;
+}
