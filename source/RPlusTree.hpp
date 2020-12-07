12a4a3b8dab82a7264e87b7676061ae40f15ea6a
italoucsp <42581127+italoucsp@users.noreply.github.com>
2020-12-07 13:35:50 -0500

    Knn implemented

--- source/RPlusTree.hpp
+++ source/RPlusTree.hpp
@@ -16,12 +16,28 @@ private:
     Entry(shared_ptr<Node> &child);
     Entry(shared_ptr<DATA_TYPE> &data, shared_ptr<Node> &child);
     HyperRectangle<N> get_mbr();
+    bool is_in_leaf();
   };
 
   template<size_t axis>
-  struct Entry_lessthan_in_anyDim {
-    bool operator() (Entry A, Entry B) {
+  struct comparator_ENTRYSINGLEDIM {
+    bool operator() (const Entry A, const Entry B) {
       return (A.get_mbr().get_boundaries().first[axis] < B.get_mbr().get_boundaries().first[axis]);
+    }
+  };
+
+  struct ENTRYDIST {
+    double distance;//Priority criteria
+    Entry entry;//Object
+    ENTRYDIST(HyperPoint &p, Entry &md_obj){
+      entry = md_obj;
+      distance = MINDIST(p, md_obj.get_mbr());//Roussopoulos said that a R tree with minimun overlap don't need MINMAXDIST, and R+ ... don't have overlap so...
+    }
+  };
+
+  struct comparator_ENTRYDIST {
+    bool operator()(const ENTRYDIST &A, const ENTRYDIST &B) {
+      return A.distance > B.distancia;
     }
   };
 
@@ -29,7 +45,7 @@ private:
     HyperRectangle<N> mbr;
     shared_ptr<Node> parent;
     ENTRY_GROUP entries;
-    bool isLeaf();
+    bool is_leaf();
 
     Node();
     void add(Entry &IR);
@@ -42,21 +58,22 @@ private:
   shared_ptr<Node> root;
 
   void hidden_insert(shared_ptr<Node> &R, Entry &IR);
-  vector<DATA_TYPE> hidden_kNN_query(DATA_TYPE refdata, size_t k, priority_queue<Entry> &q_NN);
   vector<DATA_TYPE> hidden_search(shared_ptr<Node> &R, const HyperRectangle<N> &W);
   void split_node(shared_ptr<Node> &R);
   tuple<ENTRY_GROUP, ENTRY_GROUP, ENTRY_GROUP> partition(ENTRY_GROUP *S);
   pair<COST, double> sweep(size_t axis, ENTRY_GROUP *S);
   COST calculate_cost(ENTRY_GROUP *test_set);//min coverage
-  double MINDIST(DATA_TYPE p, const HyperRectangle<N> r);
-  double MINMAXDIST(DATA_TYPE p, const HyperRectangle<N> r);
+  double MINDIST(DATA_TYPE p, const HyperRectangle<N> r);//necessary
+  double MINMAXDIST(DATA_TYPE p, const HyperRectangle<N> r);//not necessary but could be if overlap would exist
+  void push_node_in_queue(HyperPoint refdata, shared_ptr<Node> &current, priority_queue<ENTRYDIST, vector<ENTRYDIST>, comparator_ENTRYDIST> &q_NN);
+  //pack algorithm discarded because it is not really necessary, only for better partition
 
 public:
   RPlus();
   ~RPlus();
   void insert(const DATA_TYPE data);
   vector<DATA_TYPE> search(const HyperRectangle<N> &W);
-  vector<DATA_TYPE> kNN_query(DATA_TYPE refdata, size_t k);
+  vector<DATA_TYPE> kNN_query(HyperPoint refdata, size_t k);
 };
 
 //===============================R-PLUS-TREE-IMPLEMENTATION============================================
@@ -82,6 +99,7 @@ RPlus<DATA_TYPE, N, M, ff>::~RPlus() {
   root.reset();
 }
 
+//INSERTION PUBLIC
 template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
 void RPlus<DATA_TYPE, N, M, ff>::insert(const DATA_TYPE data) {
   shared_ptr<Node> R = root;
@@ -90,9 +108,10 @@ void RPlus<DATA_TYPE, N, M, ff>::insert(const DATA_TYPE data) {
   hidden_insert(R, new_entry);
 }
 
+//INSERTION PRIVATE
 template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
 void RPlus<DATA_TYPE, N, M, ff>::hidden_insert(shared_ptr<Node> &R, Entry &IR) {
-  if (!R->isLeaf()) {
+  if (!R->is_leaf()) {
     for (Entry &entry_ : R->entries) {
       if (entry_.get_mbr().contains(IR.data)) {
         hidden_insert(entry_.child, IR);
@@ -107,7 +126,7 @@ void RPlus<DATA_TYPE, N, M, ff>::hidden_insert(shared_ptr<Node> &R, Entry &IR) {
   }
 }
 
-//RANGE QUERY SEARCH
+//RANGE QUERY SEARCH PUBLIC
 template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
 vector<DATA_TYPE> RPlus<DATA_TYPE, N, M, ff>::search(const HyperRectangle<N> &W) {
   shared_ptr<Node> R = root;
@@ -118,7 +137,7 @@ vector<DATA_TYPE> RPlus<DATA_TYPE, N, M, ff>::search(const HyperRectangle<N> &W)
 template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
 vector<DATA_TYPE> RPlus<DATA_TYPE, N, M, ff>::hidden_search(shared_ptr<Node> &R, const HyperRectangle<N> &W) {
   vector<DATA_TYPE> data_found;
-  if (!R->isLeaf()) {
+  if (!R->is_leaf()) {
     for (Entry &entry : R->entries) {
       if (entry.entry_rect.isOverlaping(W)) {
         hidden_search(entry.child, W);
@@ -135,16 +154,32 @@ vector<DATA_TYPE> RPlus<DATA_TYPE, N, M, ff>::hidden_search(shared_ptr<Node> &R,
   return data_found;
 }
 
-//KNN QUERY SEARCH
+//KNN QUERY SEARCH PUBLIC AND ONLY THAT, BRANCH AND BOUND ALGORITHM
 template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
-vector<DATA_TYPE> RPlus<DATA_TYPE, N, M, ff>::kNN_query(DATA_TYPE refdata, size_t k) {
-
+vector<DATA_TYPE> RPlus<DATA_TYPE, N, M, ff>::kNN_query(HyperPoint refdata, size_t k) {
+  priority_queue<ENTRYDIST, vector<ENTRYDIST>, comparator_ENTRYDIST> best_branchs_queue;
+  vector<DATA_TYPE> kNN(k);
+  push_node_in_queue(refdata, root, best_branchs_queue);
+  size_t i = size_t(0);
+  while (i < k && !best_branchs_queue.empty()) {
+    ENTRYDIST closest_entry = best_branchs_queue.top();
+    if (!closest_entry.entry.is_in_leaf())
+      push_node_in_queue(refdata, closest_entry.entry.child, best_branchs_queue);
+    else {
+      kNN[i++] = *(closest_entry.entry.data);
+      best_branchs_queue.pop();
+    }
+  }
+  return kNN;
 }
 
-//KNN QUERY SEARCH PRIVATE
+//PUSH EACH ENTRY OF A NODE IN THE PRIORITY QUEUE
 template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
-vector<DATA_TYPE> RPlus<DATA_TYPE, N, M, ff>::hidden_kNN_query(DATA_TYPE refdata, size_t k, priority_queue<Entry> &q_NN) {
-
+void RPlus<DATA_TYPE, N, M, ff>::push_node_in_queue(HyperPoint refdata, shared_ptr<Node> &current, priority_queue<ENTRYDIST, vector<ENTRYDIST>, comparator_ENTRYDIST> &q_NN) {
+  for (size_t i = size_t(0); i < current->size; ++i) {
+    ENTRYDIST packed_entry(refdata, current->entries[i]);
+    q_NN.push(packed_entry);
+  }
 }
 
 //SPLIT NODE METHOD
@@ -156,7 +191,7 @@ void RPlus<DATA_TYPE, N, M, ff>::split_node(shared_ptr<Node> &R) {
                    R = make_shared<Node>();
                    R->add(get<0>(partitioned_group));
   for (Entry &entry : get<2>(partitioned_group)) {
-    if (R->isLeaf()) {
+    if (R->is_leaf()) {
       R->add(entry);
       N2->add(entry);
     }
@@ -180,7 +215,7 @@ void RPlus<DATA_TYPE, N, M, ff>::split_node(shared_ptr<Node> &R) {
   }
 }
 
-//PARTITION METHOD
+//PARTITION METHOD | 3 GROUPS(ONLY FIRST NODE, ONLY SECOND NODE, ENTRIES THAT ARE IN BOTH NODES)
 template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
 tuple<vector<typename RPlus<DATA_TYPE, N, M, ff>::Entry>, vector<typename RPlus<DATA_TYPE, N, M, ff>::Entry>,
     vector<typename RPlus<DATA_TYPE, N, M, ff>::Entry>> RPlus<DATA_TYPE, N, M, ff>::partition(ENTRY_GROUP *S) {
@@ -214,10 +249,10 @@ tuple<vector<typename RPlus<DATA_TYPE, N, M, ff>::Entry>, vector<typename RPlus<
   return make_tuple(s1, s2, s3);
 }
 
-//SWEEP DIMENSIONS METHOD
+//SWEEP DIMENSIONS METHOD IN ONE DIMENSION
 template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
 pair<COST, double> RPlus<DATA_TYPE, N, M, ff>::sweep(size_t axis, ENTRY_GROUP *S) {
-  Entry_lessthan_in_anyDim<axis> comparator;
+  comparator_ENTRYSINGLEDIM<axis> comparator;
   partial_sort(S->begin(), S->begin() + ff, S->end(), comparator);//sort the first ff entries to "sweep" -> O(n log m)
   ENTRY_GROUP test_set(ff);
   for (size_t id_entry = 0; id_entry < ff; test_set[id_entry] = (*S)[id_entry++]) {}//picking the ff firsts entries of the sorted set
@@ -281,8 +316,8 @@ RPlus<DATA_TYPE, N, M, ff>::Node::Node() {
 }
 
 template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
-bool RPlus<DATA_TYPE, N, M, ff>::Node::isLeaf() {
-  return child == nullptr;
+bool RPlus<DATA_TYPE, N, M, ff>::Node::is_leaf() {
+  return entries[0].is_in_leaf();
 }
 
 template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
@@ -325,4 +360,9 @@ HyperRectangle<N> RPlus<DATA_TYPE, N, M, ff>::Entry::get_mbr() {
     return child->mbr;
   }
   return HyperRectangle(data, data);
+}
+
+template<typename DATA_TYPE, size_t N, size_t M, size_t ff>
+bool RPlus<DATA_TYPE, N, M, ff>::Entry::is_in_leaf() {
+  return !child;
 }
